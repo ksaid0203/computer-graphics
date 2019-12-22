10330ee8c67717ab64f038e08ec465c7c826ccc3
KIMjuho <ksaid0203@gmail.com>
2019-12-22 03:41:14 -0800

    Juho's main

--- term/lab07-found.py
+++ term/lab07-found.py
@@ -2,73 +2,75 @@ from OpenGL.GLUT import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
 
-
 import random
 import numpy as np
 import math
 
 import Particle
 import CGEngine
+import teapot
+import cup
 
-n = 400
+n = 200
 
 def myRand(start, end) :
     interval = end - start
     return start + interval * random.random()
 
-class mySim(CGEngine.Loading) :
+def juhoDist(me, other) :
+    result = me - other
+    dist = np.linalg.norm(result)
 
+    return dist
+
+class mySim(CGEngine.Loading) :
     def __init__(self, w, h, title):
         super(mySim,self).__init__(w,h,title)
-        self.steam = []
+
         self.particle = []
         for i in range(n) :
-            #self.particle.append(Particle.Particle())
-            self.steam.append(Particle.Particle())
+            self.particle.append(Particle.Particle())
 
+        self.cup = cup.Cup()
+        self.teapot = teapot.Teapot()
         self.initObjects()
-
 
     def initObjects(self):
         root2 = math.sqrt(2.0)
         for i in range(n) :
-            ll = np.array([0.1, 0.1, 0.1])
-            vv = np.array([myRand(-2, 3), myRand(2,11), myRand(-2, 3)])
-            self.steam[i].set(ll, vv)
+            ll = np.array([0.0, 7.0, 0.0])
+            vv = np.array([0.0, 0.0, 0.0])
+            self.particle[i].set(ll, vv)
 
-            self.steam[i].setRadius(0.04)
-            self.steam[i].setGravity(np.array([0., 2.0, 0.]))
-
+            self.particle[i].setRadius(0.2)
+            self.particle[i].setGravity(np.array([0., -9.8, 0.]))
         return
-
-
 
     def frame(self):
         dt = self.getDt()
 
         super(mySim,self).frame()
 
-        for p in self.steam :
+        for p in self.particle :
             p.simulate(dt)
-            if p.loc[1] >= 6 :
-                ll = np.array([0.1, 0.1, 0.1])
-                vv = np.array([myRand(-2, 2), myRand(2,11), myRand(-2, 2)])
-                p.loc = ll
+            
+            if juhoDist(self.cup.loc, p.loc) <= 0.1 :
+                vv = np.array([0,0,0])
                 p.vel = vv
+                p.setRadius(0.06)
+                p.setGravity(np.array([0.,0.,0.]))
             p.colHandle()
 
-        glutSolidTeapot(1.0)
-
+        self.cup.draw()
+        self.teapot.draw()
+        print(self.teapot.isSkewed())
         for p in self.particle :
             p.cdraw([0.0,0.0,1.0,1.0])
-        for p in self.steam :
-            p.cdraw([1.0,1.0,1.0,1.0])
 
         super(mySim,self).afterFrame()
 
 ani = mySim(500,500, b"Lab07-3:Collision")
 ani.grid(True)
-
 
 def key(k, x, y) :
     if k == b' ':
@@ -78,7 +80,6 @@ def key(k, x, y) :
             ani.timerStart()
     if k == b'r':
         ani.initObjects()
-
 
 def draw():
     ani.frame()
